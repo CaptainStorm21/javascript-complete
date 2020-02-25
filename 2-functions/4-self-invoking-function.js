719beafe3977f3c963dbc7098964ae92b4470f8e
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-02-25 14:11:03 -0800

    Update 4-self-invoking-function.js

--- 2-functions/4-self-invoking-function.js
+++ 2-functions/4-self-invoking-function.js
@@ -4,3 +4,8 @@
   }
 )('Javascript')
 
+/*
+One good reason for using self-invoking anonymous functions is to have some work done without creating global variables. 
+A drawback, of course, is that you cannot execute the same function twice (unless you put it inside a loop or another function). 
+This makes the anonymous self-invoking functions best suited for one-off or initialization tasks.
+*/
