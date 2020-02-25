456a1ea2a1ce3a1334c47c3949df77a2fcf29f97
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-02-25 00:27:17 -0800

    Create 1-primitive-type.js

---
+++ 1-primitive-reference-type/1-primitive-type.js
@@ -0,0 +1,27 @@
+var customerName = "John Shultz";
+var accNum = 23123122;
+var isOpen = true;
+
+var object = null;
+var x = undefined;
+var y;
+
+document.write(customerName + "<br/>");
+document.write(accNum +  "<br/>");
+document.write(isOpen +  "<br/>");
+document.write(object +  "<br/>");
+document.write(x +  "<br/>");
+document.write(y + "<br/>");
+
+
+/*
+
+John Shultz
+23123122
+true
+null
+undefined
+undefined
+undefined
+
+*/
