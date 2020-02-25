5e757843dd914c685541e126bd8349f80d79536e
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-02-25 00:36:02 -0800

    Create 2-typeof.js

---
+++ 1-primitive-reference-type/2-typeof.js
@@ -0,0 +1,23 @@
+var customerName = "John Shultz";
+var accNum = 23123122;
+var isOpen = true;
+
+var object = null;
+var x = undefined;
+var y;
+
+document.write(typeof customerName + "<br/>");
+document.write(typeof accNum +  "<br/>");
+document.write(typeof isOpen +  "<br/>");
+document.write(typeof object +  "<br/>");
+document.write(typeof x +  "<br/>");
+document.write(typeof y + "<br/>");
+
+/*
+string
+number
+boolean
+object
+undefined
+undefined
+*/
